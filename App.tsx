eb1fbb265f4b797806decef62150e073c73ceb58
rolandhobaj <rolandhobaj@gmail.com>
2024-03-09 18:01:14 +0100

    Add background image

--- App.tsx
+++ App.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import {
-  SafeAreaView,
+  StyleSheet,
+  ImageBackground
 } from 'react-native';
 
 import RecipeCard from './src/RecipeCard';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <RecipeCard title='This is a recipe'/>
-      <RecipeCard title='This is a recipe'/>
-      <RecipeCard title='This is a recipe'/>
-    </SafeAreaView>
+      <ImageBackground source={require('./resources/bg.png')} resizeMode="cover" style={styles.image}>
+        <RecipeCard title='This is a recipe'/>
+        <RecipeCard title='This is a recipe'/>
+        <RecipeCard title='This is a recipe'/>
+      </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+  }
+});
+
 
 export default App;
